243e45ae898fb536a435619f8b3efef2809bda93
Maksym Rud <alice@programmingmentor.com>
2020-05-06 20:48:19 +0300

    delete from project root

--- OST/OST/settings.py
+++ OST/OST/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ostes.apps.OstesConfig',
+    'frontend.apps.FrontendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- OST/OST/urls.py
+++ OST/OST/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/ost/', include('ostes.urls'))
+    path('api/v1/ost/', include('ostes.urls')),
+    path('', include('frontend.urls')),
 ]
