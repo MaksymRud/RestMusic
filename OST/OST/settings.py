846261f83ce543e562b8d499efab753666840238
Maksym Rud <alice@programmingmentor.com>
2020-05-06 22:15:33 +0300

    node

--- OST/OST/settings.py
+++ OST/OST/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ostes.apps.OstesConfig',
-    'frontend.apps.FrontendConfig',
+    'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
