32b741f607172010f3c5980c3b9f36b06fcecc08
Maksym Rud <alice@programmingmentor.com>
2020-05-07 12:18:09 +0300

    cache

--- OST/ostes/admin.py
+++ OST/ostes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import *
+
+admin.site.register(OST)
+admin.site.register(Movie)
+admin.site.register(Season)
+admin.site.register(Episode)
+admin.site.register(Music)
+admin.site.register(EpisodeMusic)
+admin.site.register(Category)
+admin.site.register(MoviehasCategories)
+admin.site.register(Video_Preview)
+
